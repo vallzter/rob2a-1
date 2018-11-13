8c894fa06bd08b50bc84a2ba0842b91546360e8d
vallzter <valgeirtho288@nemi.tskoli.is>
2018-11-13 14:53:14 +0000

    Add files via upload

---
+++ myincludes/myincludes.c
@@ -0,0 +1,25 @@
+const int BASETURN = 3;
+const int POWER = 63;
+
+void turn(int deg,int dir){
+	while(deg*BASETURN > abs(SensorValue[leftEncoder])){
+		motor[rightMotor] = -POWER*dir;
+		motor[leftMotor]  = POWER*dir;
+	}
+
+}
+
+void stopMotors(){
+	motor[rightMotor] = 0;
+	motor[leftMotor]	= 0;
+}
+
+void resetEncoder(){
+	SensorValue[rightEncoder] = 0;
+	SensorValue[leftEncoder]	= 0;
+}
+
+void drive(){
+	motor[leftMotor] = POWER;
+	motor[rightMotor] = POWER;
+}
